804219745d280bdb5cadaafbd31a48c60b2d9854
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-10-23 09:55:58 +0200

    Fixed mistake in the namespace of the ResponseTransformer

--- src/Response/ResponseTransformerInterface.php
+++ src/Response/ResponseTransformerInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace jschreuder\Middle\Response;
+namespace jschreuder\BookmarkBureau\Response;
 
 use jschreuder\BookmarkBureau\Exception\ResponseTransformerException;
 use Psr\Http\Message\ResponseInterface;
