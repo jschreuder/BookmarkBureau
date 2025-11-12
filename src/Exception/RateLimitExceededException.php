7878cf7fac7227b78423eecb4b50c32327d92a3e
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-11-12 13:27:56 +0100

    Decided against giving too much information about why a user was rate-limited.

--- src/Exception/RateLimitExceededException.php
+++ src/Exception/RateLimitExceededException.php
@@ -11,17 +11,7 @@ final class RateLimitExceededException extends RuntimeException
         private readonly ?string $blockedIp = null,
         private readonly ?\DateTimeInterface $expiresAt = null,
     ) {
-        $message = "Rate limit exceeded. ";
-
-        if ($this->blockedUsername && $this->blockedIp) {
-            $message .= "Username and IP address are blocked.";
-        } elseif ($this->blockedUsername) {
-            $message .= "Username is blocked.";
-        } elseif ($this->blockedIp) {
-            $message .= "IP address is blocked.";
-        } else {
-            $message .= "Too many failed login attempts.";
-        }
+        $message = "Rate limit exceeded. Too many failed login attempts.";
 
         if ($this->expiresAt) {
             $message .=
