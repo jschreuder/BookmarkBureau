75bd10dd89b8c360aec1806a0f067a3c191996dd
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-12-07 23:42:46 +0100

    Not really important, but regex for dashboard_id was not working anymore... how I hate silently failing dependencies.

--- src/GeneralRoutingProvider.php
+++ src/GeneralRoutingProvider.php
@@ -178,7 +178,7 @@ final readonly class GeneralRoutingProvider implements RoutingProviderInterface
             ),
             [],
             [
-                "id" =>
+                "dashboard_id" =>
                     "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
             ],
         );
